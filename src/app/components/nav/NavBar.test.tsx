c63f410f41a412cfaeb868754bc84df1cdd26d2e
Cloudbop <boppincolin@gmail.com>
2022-01-01 19:03:16 +0000

    failtest - clicking sign-in button push 'sign-in'...

---
+++ src/app/components/nav/NavBar.test.tsx
@@ -0,0 +1,10 @@
+import { App } from "../../../App";
+import { fireEvent, render, screen } from "../../../test-utils";
+import { NavBar } from "./NavBar";
+
+test("clicking sign-in button pushes '/sign-in' to history ", () => {
+  const { history } = render(<NavBar />);
+  const signInButton = screen.getByRole("button", { name: /sign in/i });
+  fireEvent.click(signInButton);
+  expect(history.location.pathname).toBe("/sign-in");
+});
