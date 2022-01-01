e0aa46803623386892c7e1750f66294e44fe5b82
Cloudbop <boppincolin@gmail.com>
2022-01-01 19:07:46 +0000

    f-test behavioural

--- src/app/components/nav/NavBar.test.tsx
+++ src/app/components/nav/NavBar.test.tsx
@@ -2,9 +2,22 @@ import { App } from "../../../App";
 import { fireEvent, render, screen } from "../../../test-utils";
 import { NavBar } from "./NavBar";
 
+// implmentation test
 test("clicking sign-in button pushes '/sign-in' to history ", () => {
   const { history } = render(<NavBar />);
   const signInButton = screen.getByRole("button", { name: /sign in/i });
   fireEvent.click(signInButton);
+  // assert the history is updated
   expect(history.location.pathname).toBe("/signin");
+});
+
+// behavioural test
+test("Clicking sign-in button shows sign-in page", () => {
+  render(<App />);
+  const signInButton = screen.getByRole("button", { name: /sign in/i });
+  fireEvent.click(signInButton);
+  //
+  expect(
+    screen.getByRole("heading", { name: /fail me first/i })
+  ).toBeInTheDocument();
 });
