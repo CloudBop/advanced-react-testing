602f5c185c1e951a3df099cbe5337a50154bc504
Cloudbop <boppincolin@gmail.com>
2022-01-03 13:48:12 +0000

    ft- screen.getByRole find stwo elements

--- src/features/auth/tests/protected.test.tsx
+++ src/features/auth/tests/protected.test.tsx
@@ -1,3 +1,5 @@
+import userEvent from "@testing-library/user-event";
+
 import { App } from "../../../App";
 import { render, screen } from "../../../test-utils";
 
@@ -15,4 +17,23 @@ test.each([
   render(<App />, { routeHistory: [route] });
   const signInHeader = screen.getByRole("heading", { name: /sign/i });
   expect(signInHeader).toBeInTheDocument();
+});
+
+test("successful sign-in happyflowpath", () => {
+  // goto protected page
+  const { history } = render(<App />, { routeHistory: ["/tickets/1"] });
+
+  // sign in (after redirect)
+  const emailField = screen.getByLabelText(/email/i);
+  userEvent.type(emailField, "booking@avalancheofcheese.com");
+  //
+  const passwordField = screen.getByLabelText(/password/i);
+  userEvent.type(passwordField, "reduntant-string"); //  not testing server!
+
+  const signInButton = screen.getByRole("button", { name: /sign in/i });
+  userEvent.click(signInButton);
+
+  //
+  expect(history.location.pathname).toBe("/tickets/1");
+  // remove sign in from history
 });
